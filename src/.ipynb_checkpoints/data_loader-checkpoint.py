2608f0db41aa8d879c715bc83a99b4a4a238e228
Leon Morris <leon@morris2004.de>
2025-05-12 01:27:57 +0200

    Übertragen Chat code und Fehlersuche ohne Erfolg

--- src/.ipynb_checkpoints/data_loader-checkpoint.py
+++ src/.ipynb_checkpoints/data_loader-checkpoint.py
@@ -3,10 +3,8 @@ import pandas as pd
 def load_smard_cvs(url):
     # lädt SMARD CVS-Datei und bereitet sie auf
 	df = pd.read_csv(url, sep=";", decimal=',') #SMARD nutz komma als Dezimalzeichen
-    
-	df.columns = df.columns.str.strip() #Entfernt Leerzeichen
-    
+	df.colums = df.columns.str.strip() #Entfernt Leerzeichen
     df["datetime"] = pd.to_datetime(df["Datum"] + " " + df["Uhrzeit"], format="%d.%m.%Y %H:%M")
-    df = df[["datetime", "Last (MW)"]] #Nur relevante Spalten
+    df = df[["datetime", "Lasr (MW)"]] #Nur relevante Spalten
     df = df.dropna() #entfernt Zeilen mit fehlenden Werten
 	return df
